b2063b5d90724cdc0ed688fb671b28e818679285
ddfdesign2018 <43002783+ddfdesign2018@users.noreply.github.com>
2018-10-23 15:45:19 +0200

    Add files via upload

---
+++ src/main/java/com/ddfdesign/zuulmsp/ZuulMsPApplication.java
@@ -0,0 +1,18 @@
+package com.ddfdesign.zuulmsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulMsPApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ZuulMsPApplication.class, args);
+	}
+}
